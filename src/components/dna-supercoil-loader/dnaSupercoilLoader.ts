bb4704886fd9a04bc2dd398fa37283ec2e5f01c4
arcklay <seasparta618@gmail.com>
2024-01-22 23:31:12 +1100

    New-Component: finalize the dna supercoil loader

--- src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
+++ src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
@@ -1,13 +1,13 @@
 import './dna-supercoil-loader.scss';
 
-const DNA_PAIR_NUMBER=36;
+const DNA_PAIR_NUMBER=100;
 
 const buildDNASupercoilLoader = (): HTMLElement => {
     const dnaSupercoilLoader = document.createElement('div');
     dnaSupercoilLoader.className = 'dnaSupercoilLoader';
     for (let i = 0; i < DNA_PAIR_NUMBER; i++) {
         const dnaPairContainer = document.createElement('div');
-        dnaPairContainer.className = 'dnaPairContainer';
+        dnaPairContainer.className = `dnaPairContainer sequence${i}`;
         dnaSupercoilLoader.appendChild(dnaPairContainer);
     }
     return dnaSupercoilLoader;
