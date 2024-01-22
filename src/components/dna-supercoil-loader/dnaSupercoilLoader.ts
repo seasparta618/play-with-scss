e520ef269f18cfab3cb7b267c819e5d7b60c30f3
arcklay <seasparta618@gmail.com>
2024-01-22 22:27:01 +1100

    New-Component: initialize element on the circle

--- src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
+++ src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
@@ -1,8 +1,21 @@
 import './dna-supercoil-loader.scss';
 
+const DNA_PAIR_NUMBER=36;
+
 const buildDNASupercoilLoader = (): HTMLElement => {
     const dnaSupercoilLoader = document.createElement('div');
     dnaSupercoilLoader.className = 'dnaSupercoilLoader';
+    for (let i = 0; i < DNA_PAIR_NUMBER; i++) {
+        const dnaPairContainer = document.createElement('div');
+        dnaPairContainer.className = 'dnaPairContainer';
+        const dnaElementLeft = document.createElement('div');
+        dnaElementLeft.className = 'dnaElementLeft';
+        const dnaElementRight = document.createElement('div');
+        dnaElementRight.className = 'dnaElementRight';
+        dnaPairContainer.appendChild(dnaElementLeft);
+        dnaPairContainer.appendChild(dnaElementRight);
+        dnaSupercoilLoader.appendChild(dnaPairContainer);
+    }
     return dnaSupercoilLoader;
 };
 
