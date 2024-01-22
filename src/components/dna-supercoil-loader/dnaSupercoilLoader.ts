e3e09b214b717f4698d6eb6b9b875488a89bf81a
arcklay <seasparta618@gmail.com>
2024-01-22 22:53:50 +1100

    New-Component: allocate all the balls

--- src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
+++ src/components/dna-supercoil-loader/dnaSupercoilLoader.ts
@@ -8,12 +8,6 @@ const buildDNASupercoilLoader = (): HTMLElement => {
     for (let i = 0; i < DNA_PAIR_NUMBER; i++) {
         const dnaPairContainer = document.createElement('div');
         dnaPairContainer.className = 'dnaPairContainer';
-        const dnaElementLeft = document.createElement('div');
-        dnaElementLeft.className = 'dnaElementLeft';
-        const dnaElementRight = document.createElement('div');
-        dnaElementRight.className = 'dnaElementRight';
-        dnaPairContainer.appendChild(dnaElementLeft);
-        dnaPairContainer.appendChild(dnaElementRight);
         dnaSupercoilLoader.appendChild(dnaPairContainer);
     }
     return dnaSupercoilLoader;
