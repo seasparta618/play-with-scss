50dff52fa0fc76b6a31043fd5f9ba921f5af6c82
Leon Wang <seasparta618@gmail.com>
2024-02-04 21:44:05 +1100

    run format

--- src/components/buttons/moving-border-button/MovingBorderButton.ts
+++ src/components/buttons/moving-border-button/MovingBorderButton.ts
@@ -1,16 +1,16 @@
-import './moving-border-button.scss';
+import './moving-border-button.scss'
 
 const buildCyberPunkButton = (): HTMLElement => {
-    const buttonContainer = document.createElement('div');
-    buttonContainer.className="movingBorderButton-container";
-    const button = document.createElement('div');
-    button.className="movingBorderButton";
-    button.innerHTML = 'Brilliant Button';
-    buttonContainer.appendChild(button);
-    const borderProvider = document.createElement('div');
-    borderProvider.className="movingBorderButton-borderProvider";
-    buttonContainer.appendChild(borderProvider);
-    return buttonContainer;
+  const buttonContainer = document.createElement('div')
+  buttonContainer.className = 'movingBorderButton-container'
+  const button = document.createElement('div')
+  button.className = 'movingBorderButton'
+  button.innerHTML = 'Brilliant Button'
+  buttonContainer.appendChild(button)
+  const borderProvider = document.createElement('div')
+  borderProvider.className = 'movingBorderButton-borderProvider'
+  buttonContainer.appendChild(borderProvider)
+  return buttonContainer
 }
 
-export default buildCyberPunkButton;
+export default buildCyberPunkButton
