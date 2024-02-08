7877161976a13209e10a096d8be4a4aaf7ea1b22
Leon Wang <seasparta618@gmail.com>
2024-02-08 21:46:11 +1100

    New Component: add wave ball

---
+++ src/stories/components/progressive-bar/WaveBall.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import BuildWaveBall from './WaveBall'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Progressive Bar/Wave Ball',
+  component: BuildWaveBall,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    percentage: {
+      control: {type: 'number', min: 0, max: 100, step: 5},
+      description: 'Percentage of progress'
+    },
+  },
+} satisfies Meta<typeof BuildWaveBall>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    percentage: 2,
+  },
+}
---
+++ src/stories/components/progressive-bar/WaveBall.tsx
@@ -0,0 +1,38 @@
+import React,{ FC, useEffect, useRef, useState } from "react";
+import './wave-ball.scss';
+
+interface WaveballProps {
+    percentage: number;
+}
+
+const BuildWaveBall: FC<WaveballProps> = ({ percentage }) => {
+
+    const providerRef = useRef<HTMLDivElement>(null);
+
+    const [distance, setDistance] = useState<number>(0);
+
+    useEffect(()=>{
+        if (!providerRef || !providerRef.current) return
+        const refCurrent = providerRef.current
+        const computedStyle = window.getComputedStyle(refCurrent);
+        const topValue = parseInt(computedStyle.getPropertyValue('top'), 10);
+        if(topValue){
+            setDistance(topValue)
+        }
+    },[]);
+
+    useEffect(() => {
+        if (!providerRef.current) return;
+        const newTop = distance * ((100 - percentage) / 100);
+        console.log(newTop)
+        providerRef.current.style.setProperty('top', `${newTop}px`);
+    }, [percentage, distance]);
+
+    return (
+        <div className="waveBall">
+            <div className="waveBall-percentage">{percentage}%</div>
+            <div className="waveBall-provider" ref={providerRef}></div>
+        </div>)
+}
+
+export default BuildWaveBall;
